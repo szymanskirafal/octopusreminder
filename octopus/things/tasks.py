bf13fe2cbde4b7bfef3ea1fb9dbee8c558c64d28
Rafal Szymanski <r.szymansky@gmail.com>
2018-01-06 21:02:42 +0100

    minor corrects

--- octopus/things/tasks.py
+++ octopus/things/tasks.py
@@ -18,7 +18,7 @@ def task_send_email():
     user = User.objects.get(email = 'szymanski_rafal@icloud.com')
     list_of_things = Thing.objects.all().filter(created_by = user)
     subject = 'List of things to remember'
-    html_content = render_to_string('templates/things/email.html', {'things': list_of_things})
+    html_content = render_to_string('things/email.html', {'things': list_of_things})
     from_email = 'octopus@octopusreminder.com'
     to = 'r.szymansky@gmail.com'
     email = EmailMessage(subject, html_content, from_email, [to])
