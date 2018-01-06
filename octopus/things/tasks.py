c8b40215776e1a95a88fea72b7ba58b654ae4fe5
Rafal Szymanski <r.szymansky@gmail.com>
2018-01-06 21:26:28 +0100

    changed periodic task email template to today_things and later_things

--- octopus/things/tasks.py
+++ octopus/things/tasks.py
@@ -17,8 +17,12 @@ from ..users.models import User
 def task_send_email():
     user = User.objects.get(email = 'szymanski_rafal@icloud.com')
     list_of_things = Thing.objects.all().filter(created_by = user)
+    today_tings = list_of_things.filter(today = True)
+    later_things = list_of_things.filter(today = False)
     subject = 'List of things to remember'
-    html_content = render_to_string('things/email.html', {'things': list_of_things})
+    html_content = render_to_string('things/email.html', {
+        'today_things': today_things,
+        'later_things': later_things})
     from_email = 'octopus@octopusreminder.com'
     to = 'r.szymansky@gmail.com'
     email = EmailMessage(subject, html_content, from_email, [to])
